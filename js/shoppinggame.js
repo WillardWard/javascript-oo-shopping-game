7a902f3a488c961407d66b5caedc9091dca56474
Willard Ward <willardwardiii@gmail.com>
2023-03-29 18:16:33 -0500

    Checking module 4

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -256,10 +256,20 @@ function loadMasterData() {
 }
 
 // Complete this function
-const findProductById = (id) => {};
+const findProductById = (id) => {
+  let prod = new Product();
+
+  if (prod.id === id) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+  return Math.floor(Math.random() * 20 + 1);
+};
 
 const getProduct = (prodList, pId) => {
   return prodList.find(findProductById(pId));
