a7c978a2cbadad281ee963ccf1647238f4d8b5f8
Willard Ward <willardwardiii@gmail.com>
2023-03-28 19:25:15 -0500

    changed dateDiff logic

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -31,12 +31,18 @@ function Product(id, name, price, expiryDate) {
 
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {
-  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  //   const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-  const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
-  const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
+  //   const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
+  //   const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
 
-  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  //   return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+
+  let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+
+  let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  return diffDays;
 };
 
 // Here, use Object.defineProperty to create property - daysToExpire
