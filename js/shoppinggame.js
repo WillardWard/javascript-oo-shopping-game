904c3b3045718aafdfd5277a2163c031de419177
Willard Ward <willardwardiii@gmail.com>
2023-03-30 09:06:13 -0500

    Completed module 5

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -272,7 +272,9 @@ const getProduct = (prodList, pId) => {
 };
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {
+  return prod.price + tBill;
+};
 
 const findPointsToBill = (roundedTotal) => {
   if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -293,11 +295,26 @@ const findPointsToBill = (roundedTotal) => {
 };
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {
+  if (prod.daysToExpire() < 30) {
+    return 10;
+  } else {
+    return 0;
+  }
+};
 
 const calculatePoints = (prod, tBill) => {
   let pointsToBill = findPointsToBill(Math.round(tBill));
   let pointsForExpDate = findPointsForExpDate(prod);
+  player.score = player.score + pointsToBill + pointsForExpDate;
+
+  if (Object.hasOwn(prod, "points")) {
+    if (prod.isBonus) {
+      player.addPoints(prod.points);
+    } else {
+      player.deductPoints(prod.points);
+    }
+  }
 };
 
 // Complete this function
